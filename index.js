135830d445379416c2d0f0131ed8e1257cc07f82
danieltoo <torresestradadaniel@gmail.com>
2017-10-04 17:31:25 -0500

    Quité promesas inutiles

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 var ngsi = require('./lib/NGSI.js');
 
 
-ngsi.parseEntity('Room', 'Room1',{
-	temperature : 50
-}).then((jsonNgsi) => {
-	console.log(jsonNgsi)
-}).catch((err) => {
-	console.log(err)
-})
 
-ngsi.parseUpdate({
+console.log("EJEMPLO ENTITY")
+console.log(ngsi.parseEntity('Room', 'Room1',{
+	temperature : {
+		value : 50 ,
+		metadata : {
+			uno: 40,
+			dos: 56
+		}
+	}
+	
+}))
+
+console.log("EJEMPLO PARA updateEntityAttrs")
+console.log(ngsi.parseUpdate({
 	value : new Date(),
 	metadata : {
 		acurrency : 50,
 		otracosa: "Todo chido"
 	}
-}).then((update) => {
-	console.log(update)
-}).catch((err) =>{
-	console.log(err)
-})
+}))
 
-console.log(ngsi.parseValue("text"))
+console.log("EJEMPLO PARA updateJSONAttrEntity")
+
+console.log(ngsi.parseValue(new Date))
+
+
+
+
+//console.log(ngsi.parseValue("text"))
