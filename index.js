dca3671a6caf2539ba7effa492595eb1ca665310
danieltoo <torresestradadaniel@gmail.com>
2017-10-04 02:45:41 -0500

    Soporte dataTime

--- index.js
+++ index.js
@@ -5,14 +5,11 @@ var ngsi = require('./lib/NGSI.js');
 Las pruebas 
 
 */
-
-
-var metadata  = ngsi.parseJSON({
-	accuracy : 0.8
-})
-
-console.log(metadata)
-
+var d = new Date();
+var n = d.toISOString();
+var json = {}
+console.log(d.toString())
+console.log(n)
 console.log(JSON.stringify(ngsi.parseEntity('Room', 'Room1',{
 	temperature : 50,
 	humidity: {
@@ -23,13 +20,21 @@ console.log(JSON.stringify(ngsi.parseEntity('Room', 'Room1',{
 		}
 	},
 	modification : { 
-		type : "DateTime",
-		value : "Fechasss"
+		value : new Date()
 	},
 	location: {
-	    "value": "41.3763726, 2.186447514",
-	    "type": "geo:point"
+	    value: [
+	      "40.63913831188419, -8.653321266174316",
+	      "40.63881265804603, -8.653149604797363"
+	    ],
+	    type: "geo:box"
 	  }
-
 })))
+console.log("PASSS")
+console.log(ngsi.parseJSON({
+	value : d,
+	metadata : {
+		acurrency : 50
+	}
+}))
 
