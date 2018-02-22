345c3f699a55b31044e6dec47cd865a510862cab
danieltoo <torresestradadaniel@gmail.com>
2018-02-22 17:20:20 -0600

    Agregué comparativas en Query

--- index.js
+++ index.js
@@ -95,5 +95,10 @@ let coords = [
 	],
 	]
 
-let query = ngsi.createQuery({"id":"Device_Smartphone_.*","type":"Device","options":"keyValues","georel":"coveredBy","geometry":"polygon","coords":[[18.876992,-99.220316],[18.876789,-99.219189],[18.876545,-99.219243],[18.875611,-99.21972],[18.875708,-99.219897],[18.875809,-99.22008],[18.875906,-99.220171],[18.876073,-99.220187],[18.876342,-99.220171],[18.876596,-99.220262],[18.876743,-99.22031],[18.876992,-99.220316]]})
+let query = ngsi.createQuery({
+	"id":"Alert:Device_Smartphone_.*",
+	"type":"Alert",
+	"options":"keyValues",
+	"dateObserved" : ">=2018-02-20T16:54:03.931-06:00"
+	})
 console.log(query)
