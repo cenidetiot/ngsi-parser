d9be7380dd31b22a43080d427e54471c84e42a55
haidee09 <haideeram97@gmail.com>
2018-06-05 12:23:49 -0500

    Delete docs

--- docs/build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({docnames:["index"],envversion:52,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to ngsi-parser\u2019s documentation!"],titleterms:{document:0,indic:0,ngsi:0,parser:0,tabl:0,welcom:0}})
