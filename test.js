cd9b7cbe7ea2769ab7be76ab436277d7cda73550
danieltoo <torresestradadaniel@gmail.com>
2018-10-26 12:51:31 -0500

    i fix undefined possible errror

--- test.js
+++ test.js
@@ -1,3 +1,36 @@
 var ngsi  =  require("./lib/NGSI")
-var a ;
-console.log(ngsi.parseValue(a))
+var a =0 ;
+console.log(Object.prototype.toString.call(undefined))
+console.log(Object.prototype.toString.call({}))
+
+
+var attribute = ngsi.parseAttrs({
+    j : {},
+    j4: {
+        value : undefined
+    },
+
+/*    j2: {
+        type:'geo:point',
+         value: [0,1]
+    },
+    j3: {
+        value: [0,1]
+    },
+    j4: {
+        value : undefined
+    },
+    j5: {
+        type: 'geo',
+        value : undefined
+    },
+    j6: {
+        value : undefined,
+        metadata :{
+            a:3,
+            b:false
+        }
+    }, */
+
+})
+console.log(attribute)
