2c274a044dcc7abaf9297939a33351f1f6bf1d96
danieltoo <torresestradadaniel@gmail.com>
2018-10-29 11:45:31 -0600

    Update test.js

--- test.js
+++ test.js
@@ -20,26 +20,23 @@ ngsi.parseValue("ok")
 
 
 
-// Schema
+// ERORRES DEL SCHEMA
 
-//Attribute X not found in the model
-//The attribute X is required
+/* ERRORES EN DEFINICION DE LOS ATRIBUTOS  */
+//*The model is malformed need allOf or definition properties  // El atributo donde se encuentran las propiedades del schema deber ser allOf o definition
+//*The allOf atribute should be Array  // Si el atributo principal es allOf pero no es de typo array
+//The definition attribute should be JSON // Si el atributo principal es definition pero no es de tipo json
+//*Attribute X not found in the model --- Existe un atributo que no se encuentra en el Schema
 
-
-//The allOf atribute should be Array
-//The oneOf attribute should be Array
-
-//The definition attribute should be JSON
+/* ERRORES EN DEFINICION DE LOS ATTRIBUTOS REQUERIDOS  */
+//*The model is malformed need oneOf or required properties // El atributo principal de los campos requeridos debe ser un oneOf
+//*The oneOf attribute should be Array   // Si el atributo principal oneOf pero no es de typo array
+//*The attribute X is required --- Los atributos que son requeridos y no se encuentran
 //The model does not comply with the required attribute options
-//The model is malformed need oneOf or required properties
-//The model is malformed need allOf or definition properties
 
 //Actualy the only the refs supported are GSMA, Location and PhysicalObject commons
 
-//Attributes
-// X = Entity atribute value
-// A = Schema attribute 
-
+/* ERRORES EN EL VALOR DE LOS ATRIBUTOS  */
 //Error type not match X
 //Error format in date X
 //Error the minLength is A.minLength in X
